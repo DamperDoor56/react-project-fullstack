5610217d47457ad882586fa36d0294f764bbc16d
Ludmi Lopez <ludmit@outlook.com>
2022-11-05 17:12:45 -0300

    add

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from "react";
+import Counters from "./components/counters";
+import Nav from './components/nav'
+class App extends Component {
+    constructor(props){
+        super(props);
+        this.state = { };
+    }
+    render(){
+        return(
+            <React.Fragment>
+                <Nav />
+                <Counters />
+            </React.Fragment>
+        )
+    }
+}
+export default App;
