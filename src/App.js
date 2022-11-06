9b28cd8375d9e29a1ca53dc47dba25cd14c76fed
Ludmi Lopez <ludmit@outlook.com>
2022-11-06 15:12:01 -0300

    time for ds

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
 import React, { Component } from "react";
-import Counters from "./components/counters";
+import { BrowserRouter as Router, Switch, Route, Link, Redirect } from "react-router-dom";
+import Counter from "./components/counter";
+import Counters from "./components/counters"; 
 import Nav from './components/nav'
+import userData from './components/userData';
+
 class App extends Component {
     constructor(props){
         super(props);
@@ -9,7 +13,7 @@ class App extends Component {
     render(){
         return(
             <React.Fragment>
-                <Nav/>
+                <Nav />
                 <Counters />
             </React.Fragment>
         )
